ac34a79578f12ff2087e802e596d61039d810d46
ManuelGV13 <72111728+ManuelGV13@users.noreply.github.com>
2021-12-01 16:46:37 -0600

    terminado

---
+++ Práctica5/transicion.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream> 
+#include <vector> 
+
+using namespace std;
+
+class Transicion{
+	private:
+		vector<int> transiciones;
+	public:
+		void agregarTransicion(int estado);
+		bool estaVacia();
+		void imprimirElementos();
+		int obtenerTransicion(int i);
+		int obtenerTamanio();
+};

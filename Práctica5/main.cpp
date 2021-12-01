5c1bce5b7d464b8de0548f01660a265fdc484593
ManuelGV13 <72111728+ManuelGV13@users.noreply.github.com>
2021-12-01 16:41:48 -0600

    Create main.cpp
    terminado

---
+++ Práctica5/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string> 
+#include "Algoritmo.hpp"
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	Algoritmo subconjuntos;
+	subconjuntos.iniciarAlgoritmo();
+	
+	system("PAUSE");
+	return 0;
+}
